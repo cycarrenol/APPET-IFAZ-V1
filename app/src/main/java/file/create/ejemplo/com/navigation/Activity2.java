328b7ed5ea74c9d955e98129b01a818080055f13
Cristian Carreño <cycarrenol@unal.edu.co>
2018-06-23 07:16:24 -0500

    Se mejoró el guardar el archivo, ahora  lo hace con el nombre de la fecha y en una mejor ruta. Se siguieron las recomedaciones de Mariano y se mejoró el toast

--- app/src/main/java/file/create/ejemplo/com/navigation/Activity2.java
+++ app/src/main/java/file/create/ejemplo/com/navigation/Activity2.java
@@ -6,11 +6,14 @@ import android.graphics.Color;
 import android.os.Environment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Gravity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
 import java.io.File;
 import java.io.FileOutputStream;
 import java.io.FileReader;
@@ -23,6 +26,7 @@ import java.nio.Buffer;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.BufferedReader;
+import java.util.Date;
 
 public class Activity2 extends AppCompatActivity {
 
@@ -60,23 +64,35 @@ public class Activity2 extends AppCompatActivity {
             @Override
             public void onClick(View view) {
 
-                String archivo ="Miarchivo.txt";
+                Date c = Calendar.getInstance().getTime();
+
+                SimpleDateFormat df = new SimpleDateFormat("HH:mm:ss(dd-MM-yyyy)");
+                String archivo = df.format(c)+".txt";
+
+                //String archivo ="Miarchivo.txt";
                 createq.setBackgroundColor(Color.GREEN);
 
                 FileOutputStream fileOutputStream = null;
 
-                File myDirectoty1 =  new File(Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES),"Carpeta_resistividades");
+               // File myDirectoty1 =  new File(Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES),"Resistividades_APPET");
 
-                myDirectoty1.mkdir();
+
+                File myDirectoty1 =  new File(Environment.getExternalStorageDirectory(),"Resistividades");
+
+
+                // myDirectoty1.mkdir();
                 File file = new File(myDirectoty1, archivo);
 
                 Context context = getApplicationContext();
 
-                String ruta = myDirectoty1.getAbsolutePath();
+                String ruta ="Archivo guardado en: "+myDirectoty1.getName()+"/"+archivo;
+
+                //String ruta = myDirectoty1.getAbsolutePath()+"/"+archivo;
                 //  textRuta.setText(ruta);// Imprime la ruta del almacenamiento  del archivo
 
-                int duration = Toast.LENGTH_SHORT;
+                int duration = Toast.LENGTH_LONG;
                 Toast toast = Toast.makeText(context, ruta, duration);
+                toast.setGravity(Gravity.CENTER|Gravity.CENTER_HORIZONTAL,0,0);// CHANGE POSITION OF TOAST
                 toast.show();
 
                 try {
