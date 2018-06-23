24585afb250ef4e30cda210002632b288425bef9
Cristian Carreño <cycarrenol@unal.edu.co>
2018-06-23 05:30:52 -0500

    Mejoramiento de interfaz, se quita el la barra superior azul se mejora el editext  y otras funciones de interfaz

--- app/src/main/java/file/create/ejemplo/com/navigation/MainActivity.java
+++ app/src/main/java/file/create/ejemplo/com/navigation/MainActivity.java
@@ -5,10 +5,17 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Spinner;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
     private Button button;
+     EditText ETSeparacion;
+     Spinner espiner;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,16 +23,27 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         button = findViewById(R.id.button);
+        ETSeparacion = findViewById(R.id.ETSeparacion);
+        Spinner spinner = (Spinner) findViewById(R.id.spin);
+
+
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 openActivity2();
             }
         });
+
+
+
+
+
     }
 
             public void openActivity2(){
                 Intent intent = new Intent(this,Activity2.class);
                 startActivity(intent);
             }
+
+
 }
